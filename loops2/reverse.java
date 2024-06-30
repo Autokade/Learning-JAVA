169355f9acf088d3e57ded5140798ba01b9834f2
Autokade <shandilyachinmay@gmail.com>
2024-07-01 02:37:51 +0530

    first commit

---
+++ loops2/reverse.java
@@ -0,0 +1,22 @@
+
+import java.lang.*;
+import java.util.*;
+
+public class reverse{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Print a number");
+        int n = sc.nextInt();
+
+        int rev = 0, r;
+        while (n>0) {
+            r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+        }
+        System.out.println(rev);
+
+    }
+    
+}
