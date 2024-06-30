fdf2ab3f02c9abe8f52cbd7d474f51f7a93d516a
Autokade <shandilyachinmay@gmail.com>
2024-07-01 00:54:02 +0530

    first commit

---
+++ Loops/MultiplicationTable.java
@@ -0,0 +1,11 @@
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int n=5 , i;
+        for(i=1;i<11;i++){
+            int x = i*n;
+            System.out.println(x);
+        }
+    }
+    
+}
